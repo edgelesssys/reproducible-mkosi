5cccdaabebb0eab4b45fa44440bcd01190964904
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-22 11:14:36 +0200

    fedora: bootable image
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- fedora/shell.nix
+++ fedora/shell.nix
@@ -1,15 +1,17 @@
-{pkgs, ...}:
-  pkgs.mkShell {
-    nativeBuildInputs = [
-      pkgs.mkosi
+{ pkgs, ... }:
+pkgs.mkShell {
+  nativeBuildInputs = [
+    (pkgs.mkosi.override { withQemu = true; })
 
-      # package management
-      pkgs.dnf5
+    # package management
+    pkgs.dnf5
+    pkgs.rpm
 
-      # filesystem tools
-      pkgs.squashfsTools # mksquashfs
-      pkgs.dosfstools # mkfs.vfat
-      pkgs.cryptsetup # dm-verity
-      pkgs.util-linux # flock
-    ];
-  }
+    # filesystem tools
+    pkgs.btrfs-progs # mkfs.btrfs
+    pkgs.squashfsTools # mksquashfs
+    pkgs.dosfstools # mkfs.vfat
+    pkgs.cryptsetup # dm-verity
+    pkgs.util-linux # flock
+  ];
+}

d21379265b4f7d3e0f295cc7a98fee5c0e8a58e6
Malte Poll <mp@edgeless.systems>
2023-08-24 17:35:13 +0200

    mkosi: allow "seed" option

--- shells/fedora.nix
+++ shells/fedora.nix
@@ -5,8 +5,8 @@ pkgs.mkShell {
       src = fetchFromGitHub {
         owner = "malt3";
         repo = "mkosi";
-        rev = "00073162dc1c8911cc41eba14e3fd31ed33e85ed";
-        hash = "sha256-F5sDaRpneqb+RrTHUO8mOriqySpHA8Ays5UYlgGcX1c=";
+        rev = "de380587ce65903eaf09397e4dfe5ea53de0c66e";
+        hash = "sha256-LHPDfkSYQsK1HDFZHdG1jP75lzwisM6KE6cFj4VSeBQ=";
       };
     })).override
       { withQemu = true; })

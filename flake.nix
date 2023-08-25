ca57f40de2eb434e6ba52d8c37df2cc4b7f51e2e
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-25 21:51:33 +0200

    refactor fedora/ubuntu shell
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -11,11 +11,20 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+
+      mkosiDev = (pkgs.mkosi.overrideAttrs (_: rec {
+        src = pkgs.fetchFromGitHub {
+          owner = "katexochen";
+          repo = "mkosi";
+          rev = "6e61f2808cdec7f4620f9735ebbbc1ad12a1e74d";
+          hash = "sha256-2Z9AmltoIBW1qQvzlBo+iNfq+zb9UMUqy3Qkm6TMuhM=";
+        };
+      })).override { withQemu = true; };
     in
     {
       devShells."${system}" = {
-        mkosiFedora = import ./shells/fedora.nix { inherit pkgs; };
-        mkosiUbuntu = import ./shells/ubuntu.nix { inherit pkgs; };
+        mkosiFedora = import ./shells/fedora.nix { inherit pkgs mkosiDev; };
+        mkosiUbuntu = import ./shells/ubuntu.nix { inherit pkgs mkosiDev; };
       };
 
     };
--- shells/fedora.nix
+++ shells/fedora.nix
@@ -1,16 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, mkosiDev }:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
-    ((mkosi.overrideAttrs (_: rec {
-      src = fetchFromGitHub {
-        owner = "malt3";
-        repo = "mkosi";
-        rev = "de380587ce65903eaf09397e4dfe5ea53de0c66e";
-        hash = "sha256-LHPDfkSYQsK1HDFZHdG1jP75lzwisM6KE6cFj4VSeBQ=";
-      };
-    })).override
-      { withQemu = true; })
-
     # package management
     dnf5
     rpm
@@ -21,5 +11,5 @@ pkgs.mkShell {
     dosfstools # mkfs.vfat
     cryptsetup # dm-verity
     util-linux # flock
-  ];
+  ] ++ [ mkosiDev ];
 }

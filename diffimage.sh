539807da3ce2371d445dfd721ea95192b501c792
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-25 22:26:42 +0200

    update diffimage script
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- diffimage.sh
+++ diffimage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 rm -rf build*
 
 sudo mkosi --debug --distribution=fedora
@@ -8,5 +10,19 @@ sudo mkosi --debug --distribution=fedora
 
 sudo systemd-dissect --mtree build/image.raw > build/mtree
 sudo systemd-dissect --mtree build-old/image.raw > build-old/mtree
+
+for part in "root" "verity" "efi"; do
+    extract ${part} build/image.raw build/${part}
+    extract ${part} build-old/image.raw build-old/${part}
+done
+
+objcopy -O binary --only-section=.cmdline build/image.efi build/cmdline
+objcopy -O binary --only-section=.cmdline build-old/image.efi build-old/cmdline
+
+veritysetup dump build/verity
+veritysetup dump build-old/verity
+
+unzstd build/image-initrd.cpio.zst
+unzstd build-old/image-initrd.cpio.zst
 
 diff build*/mtree

6bd0f5e6b2a0a2a879d23048f38ecc80efac6e9e
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-08-31 18:52:31 +0200

    make flake multi arch
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,36 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:katexochen/nixpkgs/working";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      mkosiDev = (pkgs.mkosi.overrideAttrs (_: rec {
-        src = pkgs.fetchFromGitHub {
-          owner = "katexochen";
-          repo = "mkosi";
-          rev = "e7b6792076cd45a83f103c1df3b0b1f6d31e529c";
-          hash = "sha256-xc55is5Q0/m606Bf1P4GjUl0Vg3LMLrSC75mmo5thSs=";
+        mkosiDev = (pkgs.mkosi.overrideAttrs (_: rec {
+          src = pkgs.fetchFromGitHub {
+            owner = "katexochen";
+            repo = "mkosi";
+            rev = "e7b6792076cd45a83f103c1df3b0b1f6d31e529c";
+            hash = "sha256-xc55is5Q0/m606Bf1P4GjUl0Vg3LMLrSC75mmo5thSs=";
+          };
+        })).override {
+          # withQemu = true;
         };
-      })).override {
-        # withQemu = true;
-      };
 
-      tools = import ./tools/default.nix { inherit pkgs; };
-    in
-    {
-      devShells.${system} = {
-        mkosiFedora = import ./shells/fedora.nix { inherit pkgs mkosiDev tools; };
-        mkosiUbuntu = import ./shells/ubuntu.nix { inherit pkgs mkosiDev tools; };
-        mkosiDev = import ./shells/mkosi-dev.nix { inherit pkgs; };
-      };
+        tools = import ./tools/default.nix { inherit pkgs; };
+      in
+      {
+        devShells = {
+          mkosiFedora = import ./shells/fedora.nix { inherit pkgs mkosiDev tools; };
+          mkosiUbuntu = import ./shells/ubuntu.nix { inherit pkgs mkosiDev tools; };
+          mkosiDev = import ./shells/mkosi-dev.nix { inherit pkgs; };
+        };
 
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-    };
+        formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+      });
 }
